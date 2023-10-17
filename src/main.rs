c5c1e249c5b04084ddb11eb5db045e85f1f7520b
Sanghee Kim <sangheestyle@gmail.com>
2023-10-17 16:13:48 +0900

    a simple garphql server

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,37 @@
-use axum::{routing::get, Router};
+use std::error::Error;
+
+use async_graphql::{http::GraphiQLSource, EmptyMutation, EmptySubscription, Object, Schema};
+use async_graphql_axum::*;
+use axum::{
+    response::{self, IntoResponse},
+    routing::get,
+    Router, Server,
+};
+
+struct Query;
+
+#[Object]
+impl Query {
+    async fn howdy(&self) -> &'static str {
+        "partner"
+    }
+}
+
+async fn graphiql() -> impl IntoResponse {
+    response::Html(GraphiQLSource::build().endpoint("/").finish())
+}
 
 #[tokio::main]
-async fn main() {
-    // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+async fn main() -> Result<(), Box<dyn Error>> {
+    // create the schema
+    let schema = Schema::build(Query, EmptyMutation, EmptySubscription).finish();
+    let app = Router::new().route("/", get(graphiql).post_service(GraphQL::new(schema)));
 
-    // run it with hyper on localhost:3000
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    println!("GraphiQL: http://localhost:8000");
+
+    Server::bind(&"127.0.0.1:8000".parse().unwrap())
         .serve(app.into_make_service())
-        .await
-        .unwrap();
+        .await?;
+
+    Ok(())
 }
