d4b9e787dfe67074217bca86a89526638e2be473
Sanghee Kim <sangheestyle@gmail.com>
2023-10-24 00:03:03 +0900

    clean: warning

--- src/auth/hashing.rs
+++ src/auth/hashing.rs
@@ -1,3 +1,4 @@
+use base64::{engine::general_purpose, Engine as _};
 use rand::Rng;
 use sha2::{Digest, Sha256};
 
@@ -5,14 +6,14 @@ const SALT_LENGTH: usize = 16;
 
 pub fn generate_salt() -> String {
     let salt: [u8; SALT_LENGTH] = rand::thread_rng().gen();
-    base64::encode(&salt)
+    general_purpose::STANDARD.encode(&salt)
 }
 
 pub fn hash_password(password: &str, salt: &str) -> String {
     let mut hasher = Sha256::new();
     hasher.update(password);
     hasher.update(salt);
-    base64::encode(hasher.finalize())
+    general_purpose::STANDARD.encode(hasher.finalize())
 }
 
 pub fn verify_password(password: &str, password_hash: &str, salt: &str) -> bool {
